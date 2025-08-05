26b5d45f10eacb4dc3a4ae940df77b8e01ea790a
Aditirana1971 <aditirana1971@gmail.com>
2025-08-05 17:38:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import JobForm from './pages/JobForm';
-import CandidateUpload from './pages/CandidateUpload';
+import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
+import CandidateView from './pages/CandidateView';
 
 function App() {
   return (
     <Router>
-      <div style={{ padding: '20px' }}>
-        <h1>Internal ATS</h1>
-        <Routes>
-          <Route path="/" element={<JobForm />} />
-          <Route path="/upload" element={<CandidateUpload />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/candidate/:id" element={<CandidateView />} />
+      </Routes>
     </Router>
   );
 }
